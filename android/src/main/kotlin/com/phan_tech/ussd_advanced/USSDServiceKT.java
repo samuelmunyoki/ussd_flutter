e954b3e99219a9a0e923aeb4b90f159def70801b
David Ringle <ringunger@gmail.com>
2025-03-13 14:13:29 +0300

    Allowing Logs

--- android/src/main/kotlin/com/phan_tech/ussd_advanced/USSDServiceKT.java
+++ android/src/main/kotlin/com/phan_tech/ussd_advanced/USSDServiceKT.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.flutter.Log;
+import timber.log.Timber
 
 
 /**
@@ -42,10 +43,10 @@ public class USSDServiceKT extends AccessibilityService {
     public void onAccessibilityEvent(AccessibilityEvent event) {
         USSDServiceKT.event = event;
         USSDController ussd = USSDController.INSTANCE;
-//        Timber.d(String.format(
-//                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s",
-//                event.getEventType(), event.getClassName(), event.getPackageName(),
-//                event.getEventTime(), event.getText()));
+        Timber.d(String.format(
+                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s",
+                event.getEventType(), event.getClassName(), event.getPackageName(),
+                event.getEventTime(), event.getText()));
         if (!ussd.isRunning()) {
             return;
         }
@@ -66,11 +67,11 @@ public class USSDServiceKT extends AccessibilityService {
             clickOnButton(event, 1);
             USSDController.callbackInvoke.over(response != null ? response : "");
         } else if (isUSSDWidget(event)) {
-//            Timber.d("catch a USSD widget/Window");
+            Timber.d("catch a USSD widget/Window");
             if (notInputText(event)) {
                 // not more input panels / LAST MESSAGE
                 // sent 'OK' button
-//                Timber.d("No inputText found & closing USSD process");
+                Timber.d("No inputText found & closing USSD process");
                 clickOnButton(event, 0);
                 ussd.stopRunning();
                 USSDController.callbackInvoke.over(response != null ? response : "");
@@ -95,7 +96,12 @@ public class USSDServiceKT extends AccessibilityService {
     }
     public static void send2(String text, AccessibilityEvent ev) {
         setTextIntoField(ev, text);
-        clickOnButton(ev, 1);
+        try {
+            clickOnButton(ev, 1);
+        } catch (java.lang.Exception e) {
+            Timber.d("Error sending USSD");
+            Timber.d(e.getMessage());
+        }
     }
 
     /**
